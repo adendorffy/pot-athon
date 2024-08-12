bffe1355c7e670081f0c82a4f02499d64cef84ba
Karen Sigrid Vergeest <25387405@sun.ac.za>
2024-08-12 19:52:54 +0200

    cleaned up folder

---
+++ gen_annotations.py
@@ -0,0 +1,31 @@
+import os
+from ultralytics import YOLO
+model = YOLO('/home/karen/Desktop/HACKATHON/pot-athon/fine-tune-training/runs/detect/train19/weights/best.pt')
+test_images_dir = 'raw_test_images'
+output_annotation_dir = 'results/annotation'
+output_images_dir = 'results/images'
+
+def save_yolo_format(result, file_path):
+    with open(file_path, 'w') as f:
+        sorted_indices = result.boxes.conf.argsort(descending=True)
+        seen_classes = set()
+        for idx in sorted_indices:
+            box = result.boxes.xywh[idx]
+            cls = int(result.boxes.cls[idx].item())
+            conf = result.boxes.conf[idx].item()
+            #only take class with most confidence
+            if cls in seen_classes:
+                continue
+            else:
+                seen_classes.add(cls)
+                x_center, y_center, width, height = box.cpu().numpy() / [result.orig_shape[1], result.orig_shape[0], result.orig_shape[1], result.orig_shape[0]]
+                f.write(f"{cls} {x_center} {y_center} {width} {height}\n")
+
+# make predictions for each image and save in yolo format
+for image_name in os.listdir(test_images_dir):
+    if image_name.endswith(('.jpg', '.jpeg', '.png')):
+        image_path = os.path.join(test_images_dir, image_name)
+        results = model(image_path)
+        result = results[0]
+        annotation_file_path = os.path.join(output_annotation_dir, f"{os.path.splitext(image_name)[0]}.txt")
+        save_yolo_format(result, annotation_file_path)
--- readme.md
+++ readme.md
@@ -3,10 +3,8 @@
 1. Install requirements.txt
 2. run yolo task=detect data=data.yaml mode=train model=yolov8n.pt epochs=10 imgsz=640
    NOTE: update the file path in data.yaml
-
-1) The yolov5 model was cloned with :git clone https://github.com/ultralytics/yolov5.git
-
-2) In order to train yolov5 out dataset has to be in the format:
+   this fine tunes the model
+   In order to train yolov5 out dataset has to be in the format:
    dataset/
    ├── images/
    │ ├── train/
@@ -16,7 +14,4 @@
    │ ├── val/
 
 and the labels/annotations must be in format: <class_id> <x_center> <y_center> <width> <height>
-
-After this is done the yolov5 model can be trained using: python train.py --img 640 --batch 16 --epochs 100 --data custom_data.yaml --weights yolov5s.pt --cache
-
 1239, 1188, 1450 does not have annotation.
