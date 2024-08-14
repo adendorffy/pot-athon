d8e3208693a8574b5968f5879ebdcaf9b77b7105
FarrellMInette <farrellminette@gmail.com>
2024-08-14 11:49:48 +0200

    Training features with area estimation using mask-rcnn

--- predict.py
+++ predict.py
@@ -4,11 +4,11 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import make_scorer, mean_squared_error
 
-df = pd.read_csv('train_features.csv')
+df = pd.read_csv('train_area_features.csv')
 df = df.dropna(subset=['Bags'])
-X = df[['Width', 'Height']]
+X = df[['Area', 'Width', 'Height']]
 y = df['Bags']
-poly = PolynomialFeatures(degree=2)
+poly = PolynomialFeatures(degree=3)
 X_poly = poly.fit_transform(X)
 
 model = LinearRegression()
@@ -17,5 +17,3 @@ kf = KFold(n_splits=5, shuffle=True, random_state=42)
 mse_scorer = make_scorer(mean_squared_error)
 cv_scores = cross_val_score(model, X_poly, y, cv=kf, scoring=mse_scorer)
 print(f'Mean Cross-Validation MSE: {cv_scores.mean()}')
-
-
