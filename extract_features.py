fc8554c5808e799e69a4a476e513fb87151e8a08
= <=>
2024-08-15 13:25:51 +0200

    Predictions script using masked RCNN and linear regression with degree 2 polynomial

--- extract_features.py
+++ extract_features.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import pandas as pd
-import os
+import os, csv
 import glob
 import torch
 import torchvision
@@ -20,6 +20,9 @@ def get_stick_length_pixels(image, annotations):
     return stick_length_pixels
 
 def get_pothole_area_pixels(image, annotations):
+    mask_rcnn_model = torchvision.models.detection.maskrcnn_resnet50_fpn(weights=MaskRCNN_ResNet50_FPN_Weights.DEFAULT)
+    mask_rcnn_model.eval()
+    
     for annotation in annotations:
         if annotation.startswith("0"):
             pothole_annotation = annotation.strip().split()
@@ -43,18 +46,19 @@ def get_pothole_area_pixels(image, annotations):
 
             masks = prediction[0]['masks']
             scores = prediction[0]['scores']
-            threshold = 0.1
-
+            threshold = 0.2
+            
             filtered_masks = [masks[i] for i in range(len(scores)) if scores[i] > threshold]
-            if filtered_masks:
-                pothole_mask = filtered_masks[0][0].mul(255).byte().cpu().numpy()
-                pothole_area_pixels = np.sum(pothole_mask > 0)
+            binary_masks = [mask > 0.5 for mask in filtered_masks]
+            if len(binary_masks)>0:
+                final_mask = torch.zeros_like(binary_masks[0])
+                for mask in binary_masks: final_mask = torch.max(final_mask, mask)
+                final_mask_np = final_mask.squeeze().cpu().numpy()
+                pothole_area_pixels = np.sum(final_mask_np)
                 return pothole_area_pixels
             else: 
-                if masks.shape[0]>0:
-                    return np.sum(masks[0].mul(255).byte().cpu().numpy() > 0)
-                else: 
-                    return 1.0
+                return (height*image_height)*(width*image_width)
+                
             
 def convert_pothole_area(pothole_area_pixels, stick_area_cm, stick_area_pixels):
     pixel_to_cm_ratio = stick_area_cm / stick_area_pixels # cm^2/pix
@@ -84,48 +88,23 @@ def get_real_dimensions(image_path, annotation_path):
     annotations=None
     with open(annotation_path, 'r') as f: annotations = f.readlines()
     stick_length_pixels = get_stick_length_pixels(image, annotations) # length in pixels (area)
+    real_width, real_height = convert_pothole_height_width(stick_area, stick_length_pixels, annotations, image)
     pothole_area_pixels = get_pothole_area_pixels(image, annotations) # pothole area in pixels (area)
     pothole_area_cm2 = convert_pothole_area(pothole_area_pixels, stick_area, stick_length_pixels)
-    real_width, real_height = convert_pothole_height_width(stick_area, stick_length_pixels, annotations, image)
     
     return pothole_area_cm2, real_width, real_height
 
-image_dir = "data/train_images/train_images"
-annotation_dir = "data/train-annotations"
-labels_csv = "data/train_labels.csv"
-
-mask_rcnn_model = torchvision.models.detection.maskrcnn_resnet50_fpn(weights=MaskRCNN_ResNet50_FPN_Weights.DEFAULT)
-mask_rcnn_model.eval()
-
-labels_df = pd.read_csv(labels_csv)
-data = []
-
-for image_file in glob.glob(image_dir+"/*.jpg"):
-    image_path = os.path.join(image_file)
+def process_image(image_path, annotation_dir, labels_df):
     image_number = int((image_path.rsplit("/")[-1]).replace(".jpg", "").replace("p", ""))
-    annotation_path = annotation_dir + "/p" + str(image_number) + ".txt"
+    annotation_path = os.path.join(annotation_dir, f"p{image_number}.txt")
     pothole_area, real_width, real_height = get_real_dimensions(image_path, annotation_path)
     label_row = labels_df[labels_df['Pothole number'] == image_number]
-    if not label_row.empty:
-        bags = label_row['Bags'].values[0]
-    else:
-        bags = None       
-    data.append({
-        "ID": image_number,
-        "Area": pothole_area,
-        "Width": real_width,
-        "Height": real_height,
-        "Bags": bags
-    })
-    print({
-        "ID": image_number,
-        "Area": pothole_area,
-        "Width": real_width,
-        "Height": real_height,
-        "Bags": bags
-    })
+    bags = label_row['Bags'].values[0] if not label_row.empty else None
     
-        
-df = pd.DataFrame(data)
-output_csv = "train_area_features.csv"
-df.to_csv(output_csv, index=False)
+    return {
+        "ID": image_number,
+        "Area": pothole_area,
+        "Width": real_width,
+        "Height": real_height,
+        "Bags": bags
+    }
--- predict.py
+++ predict.py
@@ -3,17 +3,72 @@ from sklearn.model_selection import KFold, cross_val_score
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import make_scorer, mean_squared_error
+import pickle
+import glob
+from extract_features import *
 
-df = pd.read_csv('train_area_features.csv')
-df = df.dropna(subset=['Bags'])
-X = df[['Area', 'Width', 'Height']]
-y = df['Bags']
-poly = PolynomialFeatures(degree=3)
-X_poly = poly.fit_transform(X)
+def train_lr_model(df, degree=2):
 
-model = LinearRegression()
-kf = KFold(n_splits=5, shuffle=True, random_state=42)
+    df = df.dropna(subset=['Bags'])
+    X = df[['Area', 'Width', 'Height']]
+    y = df['Bags']
+    poly = PolynomialFeatures(degree=degree)
+    X_poly = poly.fit_transform(X)
 
-mse_scorer = make_scorer(mean_squared_error)
-cv_scores = cross_val_score(model, X_poly, y, cv=kf, scoring=mse_scorer)
-print(f'Mean Cross-Validation MSE: {cv_scores.mean()}')
+    model_lr = LinearRegression()
+    kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+    mse_scorer = make_scorer(mean_squared_error)
+    cv_scores = cross_val_score(model_lr, X_poly, y=y, cv=kf, scoring=mse_scorer)
+    print(f'Mean Cross-Validation MSE: {cv_scores.mean()}')
+    
+    model_lr.fit(X_poly, y)
+
+    with open('models/linear_regression_model.pkl', 'wb') as file:
+        pickle.dump(model_lr, file)
+        
+    with open('models/poly_features.pkl', 'wb') as file:
+        pickle.dump(poly, file)
+
+def evaluate_lr_model(df, degree=2):
+    with open('models/linear_regression_model.pkl', 'rb') as file:
+        model_lr = pickle.load(file)
+        
+    with open('models/poly_features.pkl', 'rb') as file:
+        poly = pickle.load(file)
+        
+    X = df[['Area', 'Width', 'Height']]
+    X_poly = poly.transform(X)
+    
+    y = model_lr.predict(X_poly)
+    df['Bags'] = y
+    df.to_csv("test_area_with_bags.csv", index=False)
+
+    return df
+
+# # Train
+# image_dir = "data/train_images/train_images"
+# annotation_dir = "data/train-annotations"
+# labels_csv = "data/train_labels.csv"
+# features_csv = "train_area_features.csv"
+
+# # Test
+image_dir = "data/test_images/test_images"
+annotation_dir = "data/test-annotations"
+labels_csv = "data/test_labels.csv"
+features_csv = "test_area_features.csv"
+
+def generate_features(image_dir, annotation_dir, labels_csv, features_csv):
+    labels_df = pd.read_csv(labels_csv)
+    data = []
+
+    for image_file in glob.glob(image_dir+"/*.jpg"):
+        result = process_image(image_file, annotation_dir, labels_df)           
+        data.append(result)
+            
+    df_test = pd.DataFrame(data)
+    df_test.to_csv(features_csv, index=False)
+
+generate_features(image_dir, annotation_dir, labels_csv, features_csv)
+train_lr_model(pd.read_csv("train_area_features.csv"), 2)
+evaluate_lr_model(pd.read_csv("test_area_features.csv"), 2)
