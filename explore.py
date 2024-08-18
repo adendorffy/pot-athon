44cd044b75f959962b69f57cde3d11eb05cb108e
AlanaVenter <alanavent82@gmail.com>
2024-08-18 13:03:07 +0200

    retraining started

---
+++ explore.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('train_area_features.csv')
+
+df = df.dropna(subset=['Bags'])
+df = df['Bags'].value_counts()
+
+plt.figure(figsize=(10, 6))
+plt.bar(df['Bags'], alpha=0.7)
+plt.title('Area vs. Bags')
+plt.xlabel('Area')
+plt.ylabel('Bags')
+plt.grid(True)
+plt.savefig('area_vs_bags.png')  # Save the plot as a PNG file
+
+
+
+
+
+
