660ed5389004437c386d72ee325b5f81d3b77fd1
AlanaVenter <alanavent82@gmail.com>
2024-08-18 15:11:02 +0200

    blah

--- explore.py
+++ explore.py
@@ -6,10 +6,10 @@ df = pd.read_csv('train_area_features.csv')
 df = df.dropna(subset=['Bags'])
 
 plt.figure(figsize=(10, 6))
-plt.scatter(df['Area'], df['Bags'], alpha=0.7)
-plt.title('Area vs. Bags')
-plt.xlabel('Area')
-plt.ylabel('Bags')
+plt.scatter(df['BoundingBoxArea'],df['Area'], alpha=0.7)
+plt.title('Area vs. Box Area')
+plt.xlabel('Box Area')
+plt.ylabel('Area')
 plt.grid(True)
 plt.savefig('area_vs_bags.png')  # Save the plot as a PNG file
 
